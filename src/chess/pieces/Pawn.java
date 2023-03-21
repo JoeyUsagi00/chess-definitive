387edd518a746e3f238813af61b6f6ed42015081
Joey Usagi <jd8092@gmail.com>
2023-03-21 17:07:07 -0300

    Knight

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -14,7 +14,7 @@ public class Pawn extends ChessPiece {
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-
+		
 		Position p = new Position(0, 0);
 
 		if (getColor() == Color.WHITE) {
@@ -27,16 +27,15 @@ public class Pawn extends ChessPiece {
 			if (getBoard().positionExists(p) && !getBoard().thereIsApiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsApiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
-			p.setValues(position.getRow() -1, position.getColumn() -1 );
+			p.setValues(position.getRow() - 1, position.getColumn() - 1);
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
-			}
-			p.setValues(position.getRow() -1, position.getColumn() +1 );
+			}			
+			p.setValues(position.getRow() - 1, position.getColumn() + 1);
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
-			}
+			}			
 		}
-		
 		else {
 			p.setValues(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsApiece(p)) {
@@ -47,19 +46,21 @@ public class Pawn extends ChessPiece {
 			if (getBoard().positionExists(p) && !getBoard().thereIsApiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsApiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
-			p.setValues(position.getRow() +1, position.getColumn() -1 );
+			p.setValues(position.getRow() + 1, position.getColumn() - 1);
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
-			}
-			p.setValues(position.getRow() +1, position.getColumn() +1 );
+			}			
+			p.setValues(position.getRow() + 1, position.getColumn() + 1);
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
-			}
+			}	
 		}
 		return mat;
 	}
-@Override
-public String toString() {
-	return "P";
-}
+	
+	@Override
+	public String toString() {
+		return "P";
+	}
+	
 }
